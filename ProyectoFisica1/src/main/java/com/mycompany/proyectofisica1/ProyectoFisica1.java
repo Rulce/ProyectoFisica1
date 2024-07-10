f399714128bed3f748360a6bbe5b23581fcb2f40
Ricardo R. <ricardoruano723@gmail.com>
2024-07-10 16:44:05 -0600

    Initial commit

---
+++ ProyectoFisica1/src/main/java/com/mycompany/proyectofisica1/ProyectoFisica1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofisica1;
+
+/**
+ *
+ * @author ricardo
+ */
+public class ProyectoFisica1 {
+
+    public static void main(String[] args) {
+ 
+        Bienvenida bi = new Bienvenida();
+        bi.setVisible(true);
+    }
+}
